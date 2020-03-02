50f108c17499577727172cb311e415036abfdf5e
MasApip <52627239+MasApip@users.noreply.github.com>
2020-03-02 19:16:13 +0700

    Update Main.php

--- src/AlwaysDay/Main.php
+++ src/AlwaysDay/Main.php
@@ -14,21 +14,3 @@ class Main extends PluginBase {
 	}
 	
 }
-
-use pocketmine\scheduler\Task;
-
-class UpdateTime extends Task {
-
-    public function __construct(Main $plugin) {
-		$this->plugin = $plugin;
-	}
-
-	public function onRun(int $currentTick) {
-		if($this->plugin->getConfig()->get("alwaysday") == true){
-			foreach($this->plugin->getServer()->getLevels() as $level){
-				$level->setTime(1000);
-			}
-		}
-		
-	}
-}
